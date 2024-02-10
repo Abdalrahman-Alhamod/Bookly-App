27149356e164b8505b3cebd69d713a7513225918
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-02-11 00:59:05 +0300

    add custom list view item to home view

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,7 @@
 class AppImages {
   static const logo = 'assets/images/Logo.png';
+  static const bookCover1 = 'assets/images/test_book1.jpg';
+  static const bookCover2 = 'assets/images/test_book2.jpg';
+  static const bookCover3 = 'assets/images/test_book3.jpeg';
+  static const bookCover4 = 'assets/images/test_book4.jpeg';
 }
