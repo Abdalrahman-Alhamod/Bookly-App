95dd6ac8e91d5dd70d3c0f587b55f18a6ffa5e10
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-02-16 23:52:22 +0300

    crea book details app bar navigate to details book view

--- lib/core/utils/app_route.dart
+++ lib/core/utils/app_route.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: constant_identifier_names
 
+import 'package:bookly/features/Home/presentation/views/book_details_view.dart';
 import 'package:bookly/features/Home/presentation/views/home_view.dart';
 import 'package:bookly/features/Splash/presentation/view/splash_view.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,7 @@ import 'package:go_router/go_router.dart';
 
 class AppRoute {
   static const kHomeView = '/home';
+  static const kBookDetailsView = '/bookDetailsView';
   static final GoRouter router = GoRouter(
     routes: <RouteBase>[
       GoRoute(
@@ -19,6 +21,12 @@ class AppRoute {
         path: kHomeView,
         builder: (BuildContext context, GoRouterState state) {
           return const HomeView();
+        },
+      ),
+       GoRoute(
+        path: kBookDetailsView,
+        builder: (BuildContext context, GoRouterState state) {
+          return const BookDetailsView();
         },
       ),
     ],
