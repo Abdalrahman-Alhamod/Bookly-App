1f0f1fd9617a765c246a2e13291d36502667abdf
Abdalrahman-Alhamod <abd.alrrahman.alhamod@gmail.com>
2024-02-17 23:25:24 +0300

    add book model

---
+++ lib/features/Home/data/book_model/epub.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class Epub extends Equatable {
+  final bool? isAvailable;
+
+  const Epub({this.isAvailable});
+
+  factory Epub.fromJson(Map<String, dynamic> json) => Epub(
+        isAvailable: json['isAvailable'] as bool?,
+      );
+
+  Map<String, dynamic> toJson() => {
+        'isAvailable': isAvailable,
+      };
+
+  Epub copyWith({
+    bool? isAvailable,
+  }) {
+    return Epub(
+      isAvailable: isAvailable ?? this.isAvailable,
+    );
+  }
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props => [isAvailable];
+}
